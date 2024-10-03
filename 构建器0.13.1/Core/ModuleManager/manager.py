01ffa5c47038c2e8691456aa4c4cf4fbe2849230
MR.JOKER <ltdove_cn@outlook.com>
2024-10-03 15:52:47 +0800

    Up

--- 构建器0.13.1/Core/ModuleManager/manager.py
+++
@@ -1,8 +0,0 @@
-def invoke_module(modulename:str,funcname:str) -> callable:
-    '''调用某模块的方法'''
-    if module := modules.get(modulename):
-        return getattr(module,funcname)
-    else:
-        raise ModuleNotFoundError
-
-modules = {}
